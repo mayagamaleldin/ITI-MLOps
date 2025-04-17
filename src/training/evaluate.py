90b352e1ed7df05976f8d8c9dcff07cb0995d644
Ezzaldin Mamdouh <kingtal632@yahoo.com>
2025-04-17 11:45:22 +0000

    configurable pipeline using hydra

--- src/training/evaluate.py
+++ src/training/evaluate.py
@@ -2,18 +2,25 @@ import json
 import os
 import pickle
 
+from omegaconf import DictConfig
 from skore import EstimatorReport
 
-MODEL_PATH = "models"
-REPORT_PATH = "reports"
 
-
-def evaluate(X_test, y_test, model_name: str, logger) -> None:
+def evaluate(X_test, y_test, cfg: DictConfig, logger) -> None:
     logger.info("loading model")
-    with open(os.path.join(MODEL_PATH, model_name, "final_model.pkl"), "rb") as pkl:
+    with open(
+        os.path.join(
+            cfg.evaluate.model_path, cfg.evaluate.model_name, "final_model.pkl"
+        ),
+        "rb",
+    ) as pkl:
         final_model = pickle.load(pkl)
     with open(
-        os.path.join(MODEL_PATH, model_name, "model_target_translator.pkl"),
+        os.path.join(
+            cfg.evaluate.model_path,
+            cfg.evaluate.model_name,
+            "model_target_translator.pkl",
+        ),
         "rb",
     ) as pkl:
         translator = pickle.load(pkl)
@@ -21,7 +28,7 @@ def evaluate(X_test, y_test, model_name: str, logger) -> None:
     final_report = EstimatorReport(final_model, X_test=X_test, y_test=y_test_enc)
     logger.info("creating evaluation report")
     evaluation_report = {
-        "model_name": model_name,
+        "model_name": cfg.evaluate.model_name,
         "estimator_name": final_report.estimator_name_,
         "fitting_time": final_report.fit_time_,
         "accuracy": final_report.metrics.accuracy(),
@@ -30,9 +37,14 @@ def evaluate(X_test, y_test, model_name: str, logger) -> None:
         "prediction_time": final_report.metrics.timings(),
     }
     logger.info("saving evaluation report")
-    if not os.path.exists(os.path.join(REPORT_PATH, model_name)):
-        os.makedirs(os.path.join(REPORT_PATH, model_name))
+    if not os.path.exists(
+        os.path.join(cfg.evaluate.reports_path, cfg.evaluate.model_name)
+    ):
+        os.makedirs(os.path.join(cfg.evaluate.reports_path, cfg.evaluate.model_name))
     with open(
-        os.path.join(REPORT_PATH, model_name, "evaluation_report.json"), "w"
+        os.path.join(
+            cfg.evaluate.reports_path, cfg.evaluate.model_name, "evaluation_report.json"
+        ),
+        "w",
     ) as js:
         json.dump(evaluation_report, js, indent=4)
